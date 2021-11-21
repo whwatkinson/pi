cd8a45f16bd49730d8b292123e2d778290fe90fd
harrywatkinson <harry@revmo.app>
2021-11-21 17:04:59 +0000

    test for pascal

--- python/pascal.py
+++ python/pascal.py
@@ -1,4 +1,5 @@
 from math import factorial
+from typing import List
 
 
 class Pascal:
@@ -11,12 +12,25 @@ class Pascal:
         return int((factorial(n)) / ((factorial(r)) * factorial(n - r)))
 
     @classmethod
-    def pascals_triangle(cls, rows: int):
+    def pascals_triangle(cls, rows: int) -> List[int]:
+        return [cls.combination(rows, r) for r in range(rows + 1)]
+
+
+class PascalORG:
+    def __init__(self, row_number):
+        self.row_number = row_number
+        self.row = self.pascals_triangle(row_number)
+
+    @staticmethod
+    def combination(n: int, r: int) -> int:
+        return int((factorial(n)) / ((factorial(r)) * factorial(n - r)))
+
+    @classmethod
+    def pascals_triangle(cls, rows: int) -> List[int]:
         result = []
         for n in range(rows):
             row = []
             for r in range(n + 1):
-
                 row.append(cls.combination(n, r))
             result.append(row)
 
---
+++ python/tests/test_pascal.py
@@ -0,0 +1,18 @@
+from pytest import mark
+
+from python.pascal import Pascal
+
+
+class TestPascal:
+    @mark.parametrize(
+        "line_number, expected_result",
+        [(0, [1]), (1, [1, 1]), (6, [1, 6, 15, 20, 15, 6, 1])],
+    )
+    def test_pascal_row(self, line_number, expected_result):
+
+        test = Pascal(line_number)
+
+        assert test.row == expected_result
+
+        if line_number > 0:
+            assert test.row[1] == line_number
