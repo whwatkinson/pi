81ad59f24a7ab98cabd81d30a5ce73c1bfdec2db
harrywatkinson <harry@revmo.app>
2021-11-21 17:32:32 +0000

    done

--- python/pascal.py
+++ python/pascal.py
@@ -14,24 +14,3 @@ class Pascal:
     @classmethod
     def pascals_triangle(cls, rows: int) -> List[int]:
         return [cls.combination(rows, r) for r in range(rows + 1)]
-
-
-class PascalORG:
-    def __init__(self, row_number):
-        self.row_number = row_number
-        self.row = self.pascals_triangle(row_number)
-
-    @staticmethod
-    def combination(n: int, r: int) -> int:
-        return int((factorial(n)) / ((factorial(r)) * factorial(n - r)))
-
-    @classmethod
-    def pascals_triangle(cls, rows: int) -> List[int]:
-        result = []
-        for n in range(rows):
-            row = []
-            for r in range(n + 1):
-                row.append(cls.combination(n, r))
-            result.append(row)
-
-        return result[rows - 1]
